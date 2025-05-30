bd8f0e08b3c412ecb0f5f04b0a5898e92da2d71c
Your Name <you@example.com>
2025-05-30 12:19:47 +0200

    Aggiungi file index.html e main.js per la terza esercitazione

---
+++ snack/snack-3/main.js
@@ -0,0 +1,15 @@
+
+let somma = 0
+
+// Loop For
+
+for (let i = 0; i < 10; i++) {
+
+
+    const numero = Number(prompt("Inserisci un numero"));
+    somma += numero
+
+
+    console.log(somma)
+
+}
